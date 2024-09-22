08dbacda24a8a15d5b9550669672a917806599bc
Carlos Morales <cmja.27@gmail.com>
2024-09-21 22:28:18 -0400

    feature: Colocado idima español por defecto y puesto ingles como secundario

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,10 +12,10 @@ export default defineConfig({
     domains: ["images.unsplash.com"],
   },
   i18n: {
-    defaultLocale: "en",
-    locales: ["en", "fr"],
+    defaultLocale: "es",
+    locales: ["es", "en"],
     fallback: {
-      fr: "en",
+      en: "es",
     },
     routing: {
       prefixDefaultLocale: false,
@@ -26,10 +26,10 @@ export default defineConfig({
     tailwind(),
     sitemap({
       i18n: {
-        defaultLocale: "en", // All urls that don't contain `fr` after `https://screwfast.uk/` will be treated as default locale, i.e. `en`
+        defaultLocale: "es", // All urls that don't contain `fr` after `https://screwfast.uk/` will be treated as default locale, i.e. `en`
         locales: {
-          en: "en", // The `defaultLocale` value must present in `locales` keys
-          fr: "fr",
+          es: "es", // The `defaultLocale` value must present in `locales` keys
+          en: "en",
         },
       },
     }),
