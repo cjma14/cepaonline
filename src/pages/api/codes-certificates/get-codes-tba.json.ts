db92b3bd1acda5f31e6f592743c453d58c11ca0c
Carlos Morales <cmja.27@gmail.com>
2024-11-27 18:41:17 -0400

    fix: Correción de filtro para buscador de codigos tba y ajuste de ortógrafia

--- src/pages/api/codes-certificates/get-codes-tba.json.ts
+++ src/pages/api/codes-certificates/get-codes-tba.json.ts
@@ -3,6 +3,7 @@ import { db, Certificate, Codes, eq, isNull, and } from "astro:db";
 import { not } from "drizzle-orm";
 
 export const GET: APIRoute = async ({ params, request }) => {
+    const searchParam = new URL(request.url).searchParams.get("search");
     const idCertificarteTBA = 14;
     return new Response(JSON.stringify({
         message: "Consuta realiza con éxito!",
@@ -11,7 +12,8 @@ export const GET: APIRoute = async ({ params, request }) => {
         .where(
             and(
                 isNull(Codes.deleted_at),
-                eq(Codes.certificate_id, idCertificarteTBA)
+                eq(Codes.certificate_id, idCertificarteTBA),
+                eq(Codes.name, searchParam)
             )
         )
         .innerJoin(Certificate, eq(Codes.certificate_id, Certificate.id))
